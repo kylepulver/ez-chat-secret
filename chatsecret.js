380fdaf80afb7e2810f920e1d8a71e17da710d1f
kp <pulverk@gmail.com>
2024-09-03 17:44:47 -0700

    fixes

--- chatsecret.js
+++ chatsecret.js
@@ -6,6 +6,9 @@ Hooks.on("renderChatMessage", async (message, html, data) => {
 
     if (!message.isRoll)
         return;
+
+    console.log(message);
+
     
     const renderRolls = async (rolls) => {
         let html = "";
@@ -17,23 +20,25 @@ Hooks.on("renderChatMessage", async (message, html, data) => {
       }
 
     const source = message._source;
-    const flavor = source.flavor.split(`data-visibility="gm"`).join("");
+    // const flavor = source.flavor.split(`data-visibility="gm"`).join("");
+    const flavor = source.flavor;
 
     let content = await renderRolls(message.rolls)
     content = $(content);
 
+    html.find(`.flavor-text`).html(flavor);
     
     if (!game.user.isGM) {
         content.find('.dice-total').html(`<span class="ez-chat-secret-text">??</span>`)
         content.find('.part-total').html(`<span class="ez-chat-secret-text">?</span>`)
         content.find('.roll.die.d20').html(`<span class="ez-chat-secret-text">?</span>`)
+        content.find('.roll.die.d20.min').removeClass("min")
+        content.find('.roll.die.d20.max').removeClass("max")
+
+        html.find('.rk').replaceWith(`<span>Recall Knowledge</span>`)
     }
 
-    content.find('.roll.die.d20.min').removeClass("min")
-    content.find('.roll.die.d20.max').removeClass("max")
-
     html.find(`.message-content`).html(content);
-    html.find(`.flavor-text`).html(flavor);
     html.find(`.action`).prepend(`<span class="ez-chat-secret-text">Secret</span> `);
     html.find('.message-sender').html(message.alias)
 })
