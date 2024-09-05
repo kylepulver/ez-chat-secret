965ed43344df72125ed52a9a17523ac376a90739
kp <pulverk@gmail.com>
2024-09-05 16:54:36 -0700

    fixes for dorako stuff

--- chatsecret.js
+++ chatsecret.js
@@ -1,32 +1,46 @@
 console.log("EZ Chat Secret | Loaded");
 
 Hooks.on("renderChatMessage", async (message, html, data) => {
-    if (!message.blind)
-        return;
-
     if (!message.isRoll)
         return;
 
-    console.log(message);
+    // Completely hide private rolls
+    if ((message.whisper || []).length == 1 && !message.blind) {
+        if (message.isAuthor|| message.whisper.includes(game.user.id)) {
+            html.find(`.action`).prepend(`<span class="ez-chat-secret-text">Whisper</span> `);
+            html.find('.dice-total').html(`<span class="ez-chat-secret-text">${html.find('.dice-total').text()}</span>`)
+            html.find('.part-total').html(`<span class="ez-chat-secret-text">${html.find('.part-total').text()}</span>`)
+            html.find('.roll.die.d20').html(`<span class="ez-chat-secret-text">${html.find('.roll.die.d20').text()}</span>`)
+            return;
+        }
+        else {
+            html.css("overflow", "hidden");
+            html.css("height", "0");
+            html.css("padding", "0");
+            html.css("margin", "0");
+            html.css("border", "0");
+            return;
+        }
+       
+    }
 
-    
+    if (!message.blind)
+        return;
+
+    // Force showbreakdown to be true
     const renderRolls = async (rolls) => {
-        let html = "";
+        let r = "";
         for ( const roll of rolls ) {
             roll.options.showBreakdown = true;
-            html += await roll.render();
+            r += await roll.render();
         }
-        return html;
+        return r;
       }
 
     const source = message._source;
-    // const flavor = source.flavor.split(`data-visibility="gm"`).join("");
-    const flavor = source.flavor;
 
     let content = await renderRolls(message.rolls)
     content = $(content);
-
-    html.find(`.flavor-text`).html(flavor);
     
     if (!game.user.isGM) {
         content.find('.dice-total').html(`<span class="ez-chat-secret-text">??</span>`)
@@ -34,11 +48,25 @@ Hooks.on("renderChatMessage", async (message, html, data) => {
         content.find('.roll.die.d20').html(`<span class="ez-chat-secret-text">?</span>`)
         content.find('.roll.die.d20.min').removeClass("min")
         content.find('.roll.die.d20.max').removeClass("max")
-
-        html.find('.rk').replaceWith(`<span>Recall Knowledge</span>`)
+    }
+    else {
+        content.find('.dice-total').html(`<span class="ez-chat-secret-text">${content.find('.dice-total').text()}</span>`)
+        content.find('.part-total').html(`<span class="ez-chat-secret-text">${content.find('.part-total').text()}</span>`)
+        content.find('.roll.die.d20').html(`<span class="ez-chat-secret-text">${content.find('.roll.die.d20').text()}</span>`)
     }
 
     html.find(`.message-content`).html(content);
+    html.find(`.message-content`).prepend(`<div class="flavor-text">${source.flavor}</div>`);
+    if (!game.user.isGM) {
+        // html.find('.rk').replaceWith(`<span class="ez-chat-secret-text">Recall Knowledge</span>`)
+        html.find('.rk .success').replaceWith(`<span class="ez-chat-secret-text">??</span>`)
+    }
+
+    console.log(message);
+
     html.find(`.action`).prepend(`<span class="ez-chat-secret-text">Secret</span> `);
     html.find('.message-sender').html(message.alias)
+
+    // Remove class that hides the character name from some Dorako thing
+    html.find('.message-sender.dorako-display-none').removeClass('dorako-display-none')
 })
